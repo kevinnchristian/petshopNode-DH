561b1402c87187681c295757b7fc0f68bc0c5dbf
kevinnchristian <kevin.cmribeiro@outlook.com>
2020-03-03 21:34:50 -0300

    Adicionar funcoes listar/buscar pets

--- index.js
+++ index.js
@@ -2,26 +2,51 @@ const http = require('http');
 const url = require('url');
 const petshop = require('./petshop');
 
-http.createServer((req, res) => {
+let server = http.createServer((req, res) => {
 
     let urlCompleta = url.parse(req.url, true);
-    console.log(urlCompleta);
+    //console.log(urlCompleta)
 
-    if (urlCompleta.pathname == "/") {
+    if(urlCompleta.pathname == '/'){
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-        res.write('Você está na página inicial!!!');
-    };
-    if (urlCompleta.pathname == "/home") {
+        res.write('Você está na página inicial');
+        res.end();
+    }
+
+    if(urlCompleta.pathname == '/home'){
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-        res.write('Você está dentro do meu sistema petshop!!!');
-    };
-    if (urlCompleta.pathname == "/pet/adicionar") {
-        if (petshop.adicionarPets(urlCompleta.query.nome)) {
+        res.write('Você está dentro do sistema petshop');
+        res.end();
+    }
+
+    if(urlCompleta.pathname == '/pet/adicionar'){
+        if(petshop.adicionarPet(urlCompleta.query.nome)){
             res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-            res.write('O pet foi cadastro com sucesso!!!');
-        }else{
-            res.write('Erro ao tentar cadastrar um pet!!!');
+            res.end('O pet foi cadastrado com sucesso!');
+        } else {
+            res.writeHead(401, {'Content-Type': 'text/html; charset=utf-8'});
+            res.end('Erro ao tentar cadastrar um pet');
         }
     }
 
-}).listen(3001, 'localhost');
+    if(urlCompleta.pathname == '/pet/listar-pets'){
+        if(petshop.listarPets()){
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+            res.end('OK!!!');
+        } else {
+            res.writeHead(401, {'Content-Type': 'text/html; charset=utf-8'});
+            res.end('Não temos pets cadastrado no sistema');
+        }
+    }
+
+    if(urlCompleta.pathname == '/pet/buscar-pets'){
+        if(petshop.buscarPet()){
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+            res.end('OK!!!');
+        } else {
+            res.writeHead(401, {'Content-Type': 'text/html; charset=utf-8'});
+            res.end('Pet não encontrado no sistema.');
+        }
+    }
+
+}).listen(3000, 'localhost');
--- petshop.js
+++ petshop.js
@@ -8,4 +8,27 @@ function adicionarPets(nomePet) {
     return true;
 };
 
-module.exports = {adicionarPets};
+function listarPets() {
+    for (let i = 0; i < listaDePets.length; i++) {
+        listaDePets;
+        console.log("Lista de pets cadastrados sendo exibida");
+    };
+    return true;
+};
+
+
+function buscarPet(nome) {
+    let nomesDaLista = [];
+    for (let nomes of listaDePets) {
+        nomesDaLista.push(nomes.nome);
+    };
+    let indiceBusca = nomesDaLista.indexOf(nome);
+    if (indiceBusca >= 0) {
+        console.log(`O nome ${nome} encontrado em nosso sistema!!!`);
+    } else {
+        console.log(`O nome ${nome} não consta no sistema.`);
+    };
+    return true;
+};
+
+module.exports = { adicionarPets, listarPets, buscarPet };
