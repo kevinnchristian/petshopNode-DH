c95e5060662224c2c50a8280a17ab8f2cbc2f063
kevinnchristian <kevin.cmribeiro@outlook.com>
2020-03-05 13:11:15 -0300

    Modificando funcoes do sistema

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const petshop = require('./petshop');
 
 let server = http.createServer((req, res) => {
 
+    // essa variavel recebe os valores da url, e o true é para função que return valor true. 
     let urlCompleta = url.parse(req.url, true);
     //console.log(urlCompleta)
 
@@ -11,42 +12,44 @@ let server = http.createServer((req, res) => {
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
         res.write('Você está na página inicial');
         res.end();
-    }
+    };
 
     if(urlCompleta.pathname == '/home'){
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-        res.write('Você está dentro do sistema petshop');
+        res.write('Você está dentro do sistema petshop!!!');
         res.end();
-    }
+    };
 
     if(urlCompleta.pathname == '/pet/adicionar'){
-        if(petshop.adicionarPet(urlCompleta.query.nome)){
+        // quando a função recebe (urlCompleta.query.nome) ao ser executada ela busca a query string passada na url,
+        // /pet/adicionar?nome=tutu esse é exemplo de url com query string.
+        if(petshop.adicionarPets(urlCompleta.query.nome)){
             res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-            res.end('O pet foi cadastrado com sucesso!');
+            res.end(`O pet ${urlCompleta.query.nome} foi cadastrado com sucesso!`);
         } else {
             res.writeHead(401, {'Content-Type': 'text/html; charset=utf-8'});
             res.end('Erro ao tentar cadastrar um pet');
         }
-    }
+    };
 
-    if(urlCompleta.pathname == '/pet/listar-pets'){
+    if(urlCompleta.pathname == '/pet/cadastros'){
         if(petshop.listarPets()){
             res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-            res.end('OK!!!');
+            res.end(`Lista de pets cadastrado até o momento: ${petshop.listaDePets}`);
         } else {
             res.writeHead(401, {'Content-Type': 'text/html; charset=utf-8'});
             res.end('Não temos pets cadastrado no sistema');
         }
-    }
+    };
 
-    if(urlCompleta.pathname == '/pet/buscar-pets'){
-        if(petshop.buscarPet()){
+    if(urlCompleta.pathname == '/pet/buscar-pet'){
+        if(petshop.buscarPet(urlCompleta.query.nome)){
             res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-            res.end('OK!!!');
+            res.end(`O pet ${urlCompleta.query.nome} foi encontrado no sistema!`);
         } else {
             res.writeHead(401, {'Content-Type': 'text/html; charset=utf-8'});
-            res.end('Pet não encontrado no sistema.');
+            res.end(`O pet ${urlCompleta.query.nome} não encontrado no sistema.`);
         }
-    }
+    };
 
-}).listen(3000, 'localhost');
+}).listen(3001, 'localhost');
--- petshop.js
+++ petshop.js
@@ -1,34 +1,33 @@
 let listaDePets = [];
 
-
+// função que adiciona o pet desejado no sistema.
 function adicionarPets(nomePet) {
-    listaDePets.push(nomePet);
+    listaDePets.push(` ${nomePet}`);
     console.log(listaDePets);
 
+    // esse return true é para mostrar a variavel urlCompleta que está ok o valor, 
+    // se retorna false a resposta sera o else apontando erro na página.
     return true;
 };
 
+// função que lista todos os pets cadastrados.
 function listarPets() {
     for (let i = 0; i < listaDePets.length; i++) {
         listaDePets;
-        console.log("Lista de pets cadastrados sendo exibida");
+        console.log("Lista de pets cadastrados sendo exibida...");
+        return true;
     };
-    return true;
 };
 
-
-function buscarPet(nome) {
-    let nomesDaLista = [];
-    for (let nomes of listaDePets) {
-        nomesDaLista.push(nomes.nome);
-    };
-    let indiceBusca = nomesDaLista.indexOf(nome);
+// função que busca pets cadastrados.
+function buscarPet(nomePet) {
+    let indiceBusca = listaDePets.indexOf(nomePet);
     if (indiceBusca >= 0) {
-        console.log(`O nome ${nome} encontrado em nosso sistema!!!`);
+        console.log(`O nome ${nomePet} encontrado em nosso sistema!!!`);
+        return true;
     } else {
-        console.log(`O nome ${nome} não consta no sistema.`);
+        console.log(`O nome ${nomePet} não consta no sistema.`);
     };
-    return true;
 };
 
-module.exports = { adicionarPets, listarPets, buscarPet };
+module.exports = { adicionarPets, buscarPet, listarPets, listaDePets };
